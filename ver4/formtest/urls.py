d2aac6414be2e432bf5a43646f1a668866919494
rtt4 <ysyyik@gmail.com>
2018-12-30 22:40:07 +0900

    Check Finder, ver1.0

--- ver4/formtest/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url('^$', views.get_name, name='get_name'),
-    url('^/your_name/$', views.your_name, name='your_name'),
-]
